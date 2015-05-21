6fbb854512ad85675f4d32626702a6680934d11d
S. Andrew Sheppard <andrew@wq.io>
2015-05-21 15:35:19 -0600

    clean up serializer; re-enable SCSS themes

--- db/geodata/serializers.py
+++ db/geodata/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from wq.db.rest.serializers import ModelSerializer
-
+from django.contrib.gis.geos import Point as GeosPoint
 from .models import Point, Observation, Attachment
 
 class AttachmentSerializer(ModelSerializer):
@@ -12,17 +12,38 @@ class AttachmentSerializer(ModelSerializer):
 
 class ObservationSerializer(ModelSerializer):
     id = serializers.ReadOnlyField()
-    
-    obsid = serializers.ReadOnlyField(required=False)
+
+    # FIXME: Why are these coming through as required?
+    obsid = serializers.ReadOnlyField()
     point = serializers.ReadOnlyField()
+    
     attachments = AttachmentSerializer(many=True)
+    
     def create(self, validated_data):
-        username = self.context['request'].user.username
+        request = self.context['request']
         attachment_data = validated_data.pop('attachments')
+        
+        # FIXME: In theory these should all be set through Fields
+        username = request.user.username
+        lat = request.POST.get('latitude', None)
+        lng = request.POST.get('longitude', None)
+
+        if lat and lng:
+            geom = GeosPoint([float(lng), float(lat)], srid=4326)
+            geom.transform(3857)
+            # FIXME: Check for existing Point at same location
+            # (need another PointField hack to make the query work)
+            pt = Point.objects.create(
+                geometry=geom,
+                userid=username
+            )
+            validated_data['point'] = pt
+            
+
         validated_data['username'] = username
         instance = super().create(validated_data)
         for attachment in attachment_data:
-            #file = attachment['data']
+            # file = attachment['data']  # Why is this empty?
             file = self.context['request'].FILES['attachments[0][data]']
             attachment = {
                 'data': file.read(),
@@ -33,6 +54,7 @@ class ObservationSerializer(ModelSerializer):
             }
             Attachment.objects.create(**attachment)
         return instance
+
     class Meta:
         model = Observation
         exclude = ['obsid', 'point']
--- db/geodata/views.py
+++ db/geodata/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from .models import Attachment, Point as ModelPoint
+from .models import Attachment
 from wq.db.rest.views import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django.contrib.gis.geos import Point as GeosPoint
 
 
 def view_image(request, attachment_id):
@@ -21,21 +20,3 @@ def view_image(request, attachment_id):
 
 class ObservationViewSet(ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
-    def perform_create(self, serializer):
-        # FIXME: Move to serializer
-        lat = self.request.POST.get('latitude', None)
-        lng = self.request.POST.get('longitude', None)
-        if lat and lng:
-            geom = GeosPoint([float(lng), float(lat)], srid=4326)
-            geom.transform(3857)
-            username = self.request.user.username
-
-            pt = ModelPoint.objects.create(
-                geometry=geom,
-                userid=username
-            )
-        else:
-            pt = None
-            
-        serializer.save(point=pt)
-        
